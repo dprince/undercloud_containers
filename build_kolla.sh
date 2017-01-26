63b7e9e6691dd0d3aafcb3f6dee7097e998fdb1b
Ian Main <imain@redhat.com>
2017-01-25 19:33:12 -0500

    Add a script to build the kolla containers with our special overrides.

---
+++ build_kolla.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -x
+
+REGISTRY=${REGISTRY:?"Please specify a registry"}
+NAMESPACE=${NAMESPACE:-"tripleo"}
+TAG=${TAG:-"latest"}
+
+cd
+git clone https://github.com/openstack/kolla.git
+cd kolla
+
+cat > template_overrides.j2 <<-EOF_CAT
+{% extends parent_template %}
+{% set base_centos_binary_packages_append = ['puppet'] %}
+{% set nova_scheduler_packages_append = ['openstack-tripleo-common'] %}
+
+# FIXME (kolla review to add ceilometer to swift proxy image)
+{% set swift_proxy_server_packages_append = ['openstack-ceilometer-common'] %}
+EOF_CAT
+
+if [ "$USER" == 'dprince' ]; then
+cat >> template_overrides.j2 <<-EOF_CAT
+{% set ironic_conductor_packages_append = ['openstack-ironic-staging-drivers', 'http://fedorapeople.org/~dprince/fedora/python-iboot-0.1.0-999.fc20.noarch.rpm'] %}
+EOF_CAT
+fi
+
+./kolla-build --base centos --template-override template-overrides.j2 --registry "$REGISTRY" --namespace "$NAMESPACE" --push --tag "$TAG"
