cfbb12ca57c6c62b36cc605cb63a840aa3df791a
Martin André <m.andre@redhat.com>
2017-02-01 17:57:01 +0100

    Customize mariadb image
    This commit disables the external repositories (mariadb.org) to use the
    packages coming from centos or RDO exclusively and installs
    mariadb-server instead of MariaDB-Galera-server.

--- build_kolla.sh
+++ build_kolla.sh
@@ -11,6 +11,12 @@ cd kolla
 
 cat > template_overrides.j2 <<-EOF_CAT
 {% extends parent_template %}
+
+# Disable external repos
+{% set base_yum_repo_files_override = [] %}
+{% set base_yum_url_packages_override = [] %}
+{% set base_yum_repo_keys_override = [] %}
+
 {% set base_centos_binary_packages_append = ['puppet'] %}
 {% set nova_scheduler_packages_append = ['openstack-tripleo-common'] %}
 
@@ -19,6 +25,15 @@ cat > template_overrides.j2 <<-EOF_CAT
 
 # FIXME (kolla review to add ceilometer to swift proxy image)
 {% set swift_proxy_server_packages_append = ['openstack-ceilometer-common'] %}
+
+# Use mariadb-server package
+{% set mariadb_packages_remove = ['MariaDB-Galera-server', 'MariaDB-client'] %}
+{% set mariadb_packages_append = ['mariadb-server'] %}
+
+# We'll configure mariadb with galera.cnf
+{% block mariadb_footer %}
+RUN rm /etc/my.cnf.d/mariadb-server.cnf /etc/my.cnf.d/auth_gssapi.cnf
+{% endblock %}
 EOF_CAT
 
 if [ "$USER" == 'dprince' ]; then
