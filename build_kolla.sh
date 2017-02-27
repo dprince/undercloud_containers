2753198597134730d071472983c2f62f1c886ab9
Dan Prince <dprince@redhat.com>
2017-02-27 15:36:44 -0500

    Updates to use upstreamed tripleo-common template overrides

--- build_kolla.sh
+++ build_kolla.sh
@@ -5,53 +5,34 @@ REGISTRY=${REGISTRY:?"Please specify a registry"}
 NAMESPACE=${NAMESPACE:-"tripleo"}
 TAG=${TAG:-"latest"}
 
-cd
-
-cat > template_overrides.j2 <<-EOF_CAT
-{% extends parent_template %}
-
-# Disable external repos
-{% set base_yum_repo_files_override = [] %}
-{% set base_yum_url_packages_override = [] %}
-{% set base_yum_repo_keys_override = [] %}
-
-{% set base_centos_binary_packages_append = ['puppet'] %}
-{% set nova_scheduler_packages_append = ['openstack-tripleo-common'] %}
-
-# Required for mistral-db-populate to load tripleo custom actions on
-# the undercloud
-{% set mistral_api_packages_append = ['openstack-tripleo-common'] %}
-{% set mistral_engine_packages_append = ['openstack-tripleo-common'] %}
-
-# NOTE: Mistral executor needs to run nova-manage cells_v2 commands on
-# the undercloud baremetal workflows.
-{% set mistral_executor_packages_append = ['openstack-tripleo-common', 'openstack-nova-common'] %}
-
-# FIXME (kolla review to add ceilometer to swift proxy image)
-{% set swift_proxy_server_packages_append = ['openstack-ceilometer-common'] %}
-
-# Use mariadb-server package
-{% set mariadb_packages_remove = ['MariaDB-Galera-server', 'MariaDB-client'] %}
-{% set mariadb_packages_append = ['mariadb-server'] %}
-
-# We'll configure mariadb with galera.cnf
-{% block mariadb_footer %}
-RUN rm /etc/my.cnf.d/mariadb-server.cnf /etc/my.cnf.d/auth_gssapi.cnf
-{% endblock %}
+# NOTE: we set this to build from master packages for TripleO
+# rather than the older RDO tested pin often which doesn't contain the latest
+cat > /tmp/kolla-build.conf <<-EOF_CAT
+[DEFAULT]
+# Comma separated list of .rpm or .repo file(s) or URL(s) to install
+# before building containers (list value)
+#rpm_setup_config = http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tested/delorean.repo,http://trunk.rdoproject.org/centos7/delorean-deps.repo
+rpm_setup_config = http://trunk.rdoproject.org/centos7/current/delorean.repo,http://trunk.rdoproject.org/centos7/delorean-deps.repo
 EOF_CAT
 
+cd
+
+curl -L -o /tmp/kolla_template_overrides.j2 http://git.openstack.org/cgit/openstack/tripleo-common/plain/contrib/tripleo_kolla_template_overrides.j2
+
+# Ironic removed drivers from the main tree so we add them
+# back here per developer
 if [ "$USER" == 'dprince' ]; then
-cat >> template_overrides.j2 <<-EOF_CAT
+cat >> /tmp/kolla_template_overrides.j2 <<-EOF_CAT
 {% set ironic_conductor_packages_append = ['openstack-ironic-staging-drivers', 'http://fedorapeople.org/~dprince/fedora/python-iboot-0.1.0-999.fc20.noarch.rpm'] %}
 EOF_CAT
 fi
 
 kolla-build \
   --base centos \
+  --config-file=/home/dprince/kolla/kolla-build.conf \
   --type binary \
   --namespace "$NAMESPACE" \
   --registry "$REGISTRY" \
   --tag "$TAG" \
-  --push \
-  --template-override template_overrides.j2 \
+  --template-override /tmp/kolla_template_overrides.j2 \
   $@
