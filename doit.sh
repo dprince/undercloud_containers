a81f9b5f82396c73a652c96d49fa109217e42dcf
Dan Prince <dprince@redhat.com>
2017-05-23 14:40:43 -0400

    Cleanup extra host state directories in cleanup.sh
    Need to do a better job at cleanup now that we store data
    in persistent host directories outside of docker volumes.

--- doit.sh
+++ doit.sh
@@ -117,6 +117,9 @@ set -x
 
 sudo docker ps -qa | xargs sudo docker rm -f
 sudo docker volume ls -q | xargs sudo docker volume rm
+sudo rm -Rf /var/lib/mysql
+sudo rm -Rf /var/lib/rabbitmq
+sudo rm -Rf /var/lib/mongodb
 EOF_CAT
 chmod 755 $HOME/cleanup.sh
 
