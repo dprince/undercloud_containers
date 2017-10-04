2beb7a7e95fa08eff910ad2f6323d274eda255b1
Ian Main <imain@redhat.com>
2017-10-04 15:37:37 -0400

    better cleanup

--- doit.sh
+++ doit.sh
@@ -141,6 +141,7 @@ sudo docker ps -qa | xargs sudo docker rm -f
 sudo docker volume ls -q | xargs sudo docker volume rm
 sudo rm -Rf /var/lib/mysql
 sudo rm -Rf /var/lib/rabbitmq
+sudo rm -Rf /var/lib/heat-config/*
 EOF_CAT
 chmod 755 $HOME/cleanup.sh
 
--- everything_should_be_clean.sh
+++ everything_should_be_clean.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -eux
 
-cd
-sudo rm -rf custom.yaml heat-agents tripleo-common python-tripleoclient run.sh tripleo-heat-templates tripleo-undercloud-passwords.yaml
+read -p "This script cleans up all your local repositories. Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cd
+    sudo rm -rf custom.yaml heat-agents tripleo-common python-tripleoclient run.sh tripleo-heat-templates tripleo-undercloud-passwords.yaml
+fi
