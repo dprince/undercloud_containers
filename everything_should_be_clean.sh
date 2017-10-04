dd86322a2ba938273ebaae3e017ae17f7bf23b8e
Ian Main <imain@redhat.com>
2017-10-04 16:46:02 -0400

    move the set below the question.

--- everything_should_be_clean.sh
+++ everything_should_be_clean.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -eux
 
 read -p "This script cleans up all your local repositories. Are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+set -eux
     cd
     sudo rm -rf custom.yaml heat-agents tripleo-common python-tripleoclient run.sh tripleo-heat-templates tripleo-undercloud-passwords.yaml
 fi
